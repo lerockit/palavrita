399570e5ecb398116dd9ebc1f7ecdf65b872852d
Leandro Morais <lerockit.dev@gmail.com>
2023-07-17 12:04:50 -0300

    test: fix image test

--- src/pages/about/about.test.tsx
+++ src/pages/about/about.test.tsx
@@ -1,6 +1,10 @@
 import { render } from '@testing-library/react'
 import About from '.'
 
+jest.mock('../../images/about_picture.png', () => ({
+  default: '',
+}))
+
 //Just for coverage
 describe('<About />', () => {
   it('Should render', () => {
